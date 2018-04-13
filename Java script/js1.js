f77ffb6d047b8ad6c00752be53a22ea72ba5ea00
LY <572772285@qq.com>
2018-04-13 21:19:03 +0800

    4-13

---
+++ Java script/js1.js
@@ -0,0 +1,15 @@
+		// window.onload = function(){
+		// 	function add(){
+		// 	document.getElementById('box').style.background='red';
+		// 	alert(123);
+		// 	}
+		// 	document.getElementById('box').onclick=add
+		// }
+			window.onload = function(){
+			
+			document.getElementById('box').onclick= function(){
+				document.getElementById('box1').style.background='blue';
+				alert(123);
+			}
+		}
+		

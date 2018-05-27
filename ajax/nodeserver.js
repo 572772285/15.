bdc74d093f3ecd86389d092dcee4c90fc3b4c872
LY <572772285@qq.com>
2018-05-27 17:12:04 +0800

    5-27

--- ajax/nodeserver.js
+++ ajax/nodeserver.js
@@ -2,6 +2,8 @@ var http=require('http');
 var fs=require('fs');
 var server=http.createServer(function(req,res){
 	res.setHeader('Content-Type','text/html;charset=UTF-8');
+	res.setHeader('Access-Control-Allow-Origin','http://localhost:3000');
+	res.setHeader('Access-Control-Allow-headers','text');
 	var pathFile='./'+req.url;
 	fs.readFile(pathFile,function(err,data){
 		if(err){
@@ -12,7 +14,6 @@ var server=http.createServer(function(req,res){
 			res.end(data);
 		}
 	})
-	
 	// res.end('<h1>hello,liu,你好....</h>');
 });
 server.listen(3000,'127.0.0.1',function(){

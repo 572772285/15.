72e00a6f2e6a33ec167cb7f1fe5c418f7d846d41
LY <572772285@qq.com>
2018-06-12 18:01:02 +0800

    6-12

---
+++ taobao/js/index.js
@@ -0,0 +1,14 @@
+// console.log($('.fl'))
+
+
+
+$(function(){
+	$('.dropdown').hover(function(){
+		var $this=$(this);
+		console.log($this.data('active'))
+		// var menu=$this.data('active')
+		$(this).addClass($this.data('active')+'-active')
+	},function(){
+		$(this).removeClass($(this).data('active')+'-active')
+	})
+})

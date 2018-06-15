cda30d8b5b2ebd938717bc5e745efbc08846cf5c
LY <572772285@qq.com>
2018-06-15 20:44:07 +0800

    6-15

--- taobao/js/index.js
+++ taobao/js/index.js
@@ -3,13 +3,13 @@
 
 
 $(function(){
-	$('.dropdown').hover(function(){
-		var $this=$(this);
-		console.log($this.data('active'))
-		// var menu=$this.data('active')
-		$(this).addClass($this.data('active')+'-active')
-	},function(){
-		$(this).removeClass($(this).data('active')+'-active')
-	})
+		$('.dropdown').hover(function(){
+			var $this=$(this);
+			console.log($this.data('active'))
+			// var menu=$this.data('active')
+			$(this).addClass($this.data('active')+'-active')
+		},function(){
+			$(this).removeClass($(this).data('active')+'-active')
+		})
 
 })
--- taobao/js/search-object.js
+++ taobao/js/search-object.js
@@ -1,5 +1,27 @@
-function search($elem,option){
-	this.$elem=$elem;
-	this.$searchInput=$searchInput;
-	this.$submit=$submit;
-}
+		var $searchForm=$('#search-form')
+		var $searchInput=$('.search-input');
+		var $submit=$('.submit');
+		var $searchLayer=$('.searchLayer')
+
+(function($){
+	function Search($elem,option){
+		this.$elem=$elem;
+		this.$searchInput=$searchInput;
+		this.$submit=$submit;
+	}
+	Search.prototype={
+		constructor:Search,
+		_init:function(){
+
+		}
+	}
+	$.fn.extend({
+			Search:function(option){
+				return this.each(function(){
+					var $this=$(this),
+					var search=$this.data('search')
+				})
+				
+			}
+	})
+})(jQuery)

2b45c271b4ddb3c369cb32509890c328c78d52bc
LY <572772285@qq.com>
2018-07-04 18:02:08 +0800

    7-4

--- taobao/js/index.js
+++ taobao/js/index.js
@@ -1,15 +1,32 @@
-// console.log($('.fl'))
-
-
-
-$(function(){
-		$('.dropdown').hover(function(){
-			var $this=$(this);
-			console.log($this.data('active'))
-			// var menu=$this.data('active')
-			$(this).addClass($this.data('active')+'-active')
-		},function(){
-			$(this).removeClass($(this).data('active')+'-active')
+(function($){
+	var $dropdown=$('.dropdown');
+	$dropdown.on('dropdown-show',function(ev){
+		// console.log(this);
+		var $this=$(this);
+		//获取需要请求的地址
+		var loadUrl=$this.data('load');
+		var isLoaded=$this.data('isLoaded');
+		//如果页面没有设置请求地址直接返回
+		if(!loadUrl) return;
+		//如果加载过了就直接返回
+		if(isLoaded) return;
+		//如果有data-load这个请求地址就请求数据
+		$.getJSON(loadUrl,function(data){
+			// console.log(data)
+			var html='';
+			for(var i=0; i<data.length;i++){
+				html+='<li><a href="'+data[i].url+'"  class="dropdown-item bg">'+data[i].name+'</a></li>';
+				setTimeout(function(){
+					$this.find('.dropdown-layer').html(html);
+					$this.data('isLoaded',true);
+				},1000)
+				
+			}
 		})
-
-})
+	})
+	$dropdown.dropdown({});
+	// $('button').eq(0).on('click',function(){
+	// 	$dropdown.dropdown('show')
+	// })
+	
+})(jQuery);

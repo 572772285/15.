40385ac768b35267ecf018d2b4e952b3a7642ea0
LY <572772285@qq.com>
2018-07-23 17:35:50 +0800

    7-23

--- nodejs/02定时器/timer.js
+++ nodejs/02定时器/timer.js
@@ -25,6 +25,7 @@ t()
 console.log(3)
 */
 
+/*
 //同步定时器是异步，如果清掉定时器则不会执行
 console.log(1)
 let t=()=>{
@@ -32,6 +33,18 @@ let t=()=>{
 		console.log(2)
 	},1000)
 	clearInterval(t1)
+}
+t()
+console.log(3)
+*/
+
+//同步定时器是异步，如果清掉定时器则不会执行
+console.log(1)
+let t=()=>{
+	let t1=setImmediate(()=>{
+		console.log(2)
+	},1000)
+	// clearImmediate(t1)
 }
 t()
 console.log(3)
